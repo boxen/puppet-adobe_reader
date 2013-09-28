fbc78fb089300165dea47943a0555deb13ec6384
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-09-28 21:05:31 +0200

    Update to latest version

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class adobe_reader {
   package { 'AdobeReader.pkg':
     provider => 'pkgdmg',
-    source   => 'http://ardownload.adobe.com/pub/adobe/reader/mac/11.x/11.0.02/en_US/AdbeRdr11002_en_US.dmg',
+    source   => 'http://ardownload.adobe.com/pub/adobe/reader/mac/11.x/11.0.04/en_US/AdbeRdr11004_en_US.dmg',
   }
 }
--- spec/classes/adobe_reader_spec.rb
+++ spec/classes/adobe_reader_spec.rb
@@ -4,6 +4,6 @@ describe 'adobe_reader' do
 
   it { should contain_class('adobe_reader') }
   it { should contain_package('AdobeReader.pkg').with_provider('pkgdmg') }
-  it { should contain_package('AdobeReader.pkg').with_source('http://ardownload.adobe.com/pub/adobe/reader/mac/11.x/11.0.02/en_US/AdbeRdr11002_en_US.dmg') }
+  it { should contain_package('AdobeReader.pkg').with_source('http://ardownload.adobe.com/pub/adobe/reader/mac/11.x/11.0.04/en_US/AdbeRdr11004_en_US.dmg') }
 
 end
